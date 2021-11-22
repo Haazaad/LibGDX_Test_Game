91f441752eceba3795b72da51f5e25b893629952
Artem <smirnov.artem.sam@gmail.com>
2021-11-22 11:40:14 +0400

    +Добавлено плавное перемещение картинки в заданную точку

--- core/src/ru/haazad/stargame/screen/impl/MenuScreen.java
+++ core/src/ru/haazad/stargame/screen/impl/MenuScreen.java
@@ -7,29 +7,29 @@ import com.badlogic.gdx.math.Vector2;
 import ru.haazad.stargame.screen.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
+    private static float SPEED = 2f;
+
     private Texture img;
-    private Vector2 touch;
-    private Vector2 v;
     private Vector2 position;
+    private Vector2 velocity;
+    private Vector2 direction;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
-        touch = new Vector2();
-        v = new Vector2(1, 1);
-        position = new Vector2(0, 0);
+        position = new Vector2();
+        velocity = new Vector2(1, 1);
+        direction = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         batch.begin();
-        batch.draw(img, touch.x, touch.y);
+        move();
+        batch.draw(img, position.x, position.y);
         batch.end();
-        if (touch != position) {
-            touch.set(position);
-        }
     }
 
     @Override
@@ -40,8 +40,13 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        position.set(screenX - img.getWidth() / 2, Gdx.graphics.getHeight() - screenY - img.getHeight() / 2);
+        direction.set(screenX - (float) img.getWidth() / 2, Gdx.graphics.getHeight() - screenY - (float) img.getHeight() / 2);
         return super.touchDown(screenX, screenY, pointer, button);
+    }
+
+    private void move() {
+        velocity = direction.cpy().sub(position).nor().scl(SPEED);
+        position.add(velocity);
     }
 
 }
